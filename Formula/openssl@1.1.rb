cf223f351ff15f80d6d5bd172325518c4e453a7e
UnitySteveHe <steve.he@unity3d.com>
2024-10-31 10:38:43 -0500

    avoid creating symlink

--- Formula/openssl@1.1.rb
+++ Formula/openssl@1.1.rb
@@ -4,6 +4,8 @@ class OpensslAT11 < Formula
   url "https://github.com/openssl/openssl/releases/download/OpenSSL_1_1_1w/openssl-1.1.1w.tar.gz"
   sha256 "cf3098950cb4d853ad95c0841f1f9c6d3dc102dccfcacd521d93925208b76ac8"
 
+  keg_only :versioned_formula  # This prevents symlink creation in /opt/homebrew/bin
+
   def install
     system "./config --prefix=#{prefix} --openssldir=#{etc}/openssl@1.1"
     system "make"
@@ -13,7 +15,8 @@ class OpensslAT11 < Formula
   def caveats
     <<~EOS
       OpenSSL 1.1 has been installed in #{opt_prefix}.
-      Add to PATH with:
+      It is not symlinked to the default Homebrew bin path due to potential conflicts with other versions.
+      To use it, add it to your PATH:
         export PATH=#{opt_bin}:$PATH
     EOS
   end
